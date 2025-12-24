8fe2e94ac52935a4ff49060ec22da59f17e02bb7
IANO <iano@localhost>
2025-12-24 12:27:33 +0300

    Save local edits

--- src/server.ts
+++ src/server.ts
@@ -45,6 +45,20 @@ class AppServer {
             console.log(`Server running on http://localhost:${this.port}`);
         });
     }
+
+    private parseBody(req: IncomingMessage): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let body = "";
+            req.on("data", chunk => (body += chunk.toString()))
+            req.on("end", () => {
+                try {
+                    resolve(body ? JSON.parse(body) : {});
+                }catch (error) {
+                    reject(error);
+                }
+            });
+        });    
+    }
 }
 
 const server = new AppServer(5000);
