b07fc38d1ab64ae8c00d802883e3711d3bf23b58
IANO <iano@localhost>
2025-12-27 21:43:26 +0300

    Save local edits

--- src/server.ts
+++ src/server.ts
@@ -29,11 +29,14 @@ class AppServer {
             if (pathname === "/users" && req.method === "GET") {
                 return this.getAllUsers(res);
             }
+            if (pathname.startsWith("/users/") && req.method === "GET") {
+                return this.getUser(req, res, pathname);
+            }
             if (pathname.startsWith("/users/") && req.method === "PUT") {
-                // TODO
+                return this.updateUser(req, res, pathname);
             }
             if (pathname.startsWith("/users/") && req.method === "DELETE") {
-                // TODO
+                return this.deleteUser(res, pathname);
             }
 
             this.sendJSON(res, 404, { error: "Route not found" });
@@ -91,6 +94,46 @@ class AppServer {
 
     private async getAllUsers(res: ServerResponse) {
         this.sendJSON(res, 200, this.users);
+    }
+
+    private async getUser(req: IncomingMessage, res: ServerResponse, pathname: string){
+        const id = parseInt(pathname.split("/")[2] || "0");
+        const user = this.users.find((user) => user.id === id);
+        user
+            ? this.sendJSON(res, 200, user)
+            : this.sendJSON(res, 404, { error: "User not found" });
+    }
+
+    private async updateUser(req: IncomingMessage, res: ServerResponse, pathname: string){
+        try {
+            const id = parseInt(pathname.split("/")[2] || "0");
+            const body = await this.parseBody(req);
+
+            const user = this.users.find((user) => user.id === id);
+            if (!user) {
+                return this.sendJSON(res, 404, { error: "User not found" });
+            }
+
+            if (body.name){
+                user.name = body.name;
+            }
+            if (body.email){
+                user.email = body.email;
+            }
+            this.sendJSON(res, 200, user);
+        }catch (error) {
+            this.sendJSON(res, 400, { error: "Invalid JSON format" });
+        }
+    }
+
+    private async deleteUser(res: ServerResponse, pathname: string) {
+        const id = parseInt(pathname.split("/")[2] || "0");
+        const index = this.users.findIndex((user) => user.id === id);
+        if (index === -1) {
+            return this.sendJSON(res, 404, { error: "User not found" });
+        }
+        const deletedUser = this.users.splice(index, 1)[0];
+        this.sendJSON(res, 200, {message: "User deleted", data: deletedUser});
     }
 }
 
